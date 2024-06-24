3a8dc614e07ffa4440d6b72994f65012dab9e2f8
kakakayano <kakakayano815@gmail.com>
2024-06-24 13:51:48 +0900

    3 days ago What am i created

---
+++ react/my-app/src/sampleNames.jsx
@@ -0,0 +1,14 @@
+//************************************************
+
+//子要素のひな形
+import React from 'react';
+
+const SampleName = () => {
+    return (
+      <div>
+        son create !!!
+      </div>
+    )
+  }
+  
+  export default SampleComponent;//ここで親が呼び出せるようにする
